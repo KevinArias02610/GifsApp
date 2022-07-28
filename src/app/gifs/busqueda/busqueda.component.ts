a34c514dbda3e388836d7ec4f9674025cea0158e
julianarias <kjulianr41@gmail.com>
2022-07-28 18:43:58 -0500

    KJAR - Control de historial de busquedas

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,20 +1,19 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
-  templateUrl: './busqueda.component.html'
+  templateUrl: './busqueda.component.html',
 })
 export class BusquedaComponent {
-
   @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+  
+  constructor(private gifsService: GifsService) {}
 
-  buscar(){
+  buscar() {
     const valor = this.txtBuscar.nativeElement.value;
-
-    console.log(valor);
+    this.gifsService.buscarGifs(valor);
 
     this.txtBuscar.nativeElement.value = '';
-    
   }
-
 }
---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GifsService {
+  private _historial: string[] = [];
+  private apiKey: string = '9P1Xzy2G77FWcx4ZPfIqO9C75DeKCKRQ';
+
+  get historial() {
+    return [...this._historial];
+  }
+
+  buscarGifs(query: string) {
+    query = query.trim().toLowerCase();
+    if (!this._historial.includes(query)) {
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0, 10);
+    }
+    console.log(this._historial);
+  }
+}
