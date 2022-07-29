87e59c7b436e5b4ff6218fedf7c43696bb1d2810
julianarias <kjulianr41@gmail.com>
2022-07-28 19:01:00 -0500

    Sección 7 Clase 85 (Peticiones HTTP)

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -11,12 +12,21 @@ export class GifsService {
     return [...this._historial];
   }
 
+  constructor(
+    private http: HttpClient
+  ){ }
+
   buscarGifs(query: string) {
     query = query.trim().toLowerCase();
     if (!this._historial.includes(query)) {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10);
-    }
-    console.log(this._historial);
+    };
+
+    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=9P1Xzy2G77FWcx4ZPfIqO9C75DeKCKRQ&q=dragon ball z&limit=10').subscribe((res: any) => {
+      console.log(res.data)
+    })
+
+
   }
 }
